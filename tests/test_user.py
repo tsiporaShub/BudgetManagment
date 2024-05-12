0d27697d6e1147a45b2c9ae48885565c242f0879
הנדסת תוכנה <huser@sem.local>
2024-05-12 18:50:16 +0300

    add test_login function & test_updateDetails function

--- tests/test_user.py
+++ tests/test_user.py
@@ -13,23 +13,58 @@ def test_signup():
         "age": 19,
         "city": "Modiin Elit"
     }
-    response = requests.post(BASE_URL+"user/signup", data=json.dumps(data))
+    response = requests.post(BASE_URL + "user/signup", data=json.dumps(data))
     assert response.status_code == 200
     data["name"] = 'a'
-    response = requests.post(BASE_URL+"user/signup", data=json.dumps(data))
+    response = requests.post(BASE_URL + "user/signup", data=json.dumps(data))
     assert response.status_code == 422
     data["name"] = 'Tzipi'
-    data["password"] = 123
-    response = requests.post(BASE_URL+"user/signup", data=json.dumps(data))
+    data["password"] = '123'
+    response = requests.post(BASE_URL + "user/signup", data=json.dumps(data))
     assert response.status_code == 422
     data["password"] = "12345678"
     data["email"] = '101010.com'
-    response = requests.post(BASE_URL+"user/signup", data=json.dumps(data))
+    response = requests.post(BASE_URL + "user/signup", data=json.dumps(data))
     assert response.status_code == 422
     data["email"] = 't101010@gmail.com'
     data["age"] = -5
-    response = requests.post(BASE_URL+"user/signup", data=json.dumps(data))
+    response = requests.post(BASE_URL + "user/signup", data=json.dumps(data))
     assert response.status_code == 422
     data["age"] = 19
-    response = requests.post(BASE_URL+"user/signup", data=json.dumps(data))
+    response = requests.post(BASE_URL + "user/signup", data=json.dumps(data))
     assert response.status_code == 409
+
+
+def test_login():
+    data = {
+        "name": "Tzipi",
+        "password": "12345678"
+    }
+    response = requests.post(BASE_URL + "user/login", data=json.dumps(data))
+    assert response.status_code == 200
+    assert response.json() == 0
+    data["name"] = 'a'
+    data["password"] = '123'
+    response = requests.post(BASE_URL + "user/login", data=json.dumps(data))
+    assert response.status_code == 404
+
+
+def test_updateDetails():
+    userId = 0
+    data = {
+        "id": 0,
+        "name": "Tzipi",
+        "password": "123456",
+        "email": "t2222@gmail.com",
+        "age": 20,
+        "city": "Jerusalem"
+    }
+    response = requests.put(BASE_URL + f"user/updateDetails/{userId}", data=json.dumps(data))
+    assert response.status_code == 200
+    userId = -100
+    response = requests.put(BASE_URL + f"user/updateDetails/{userId}", data=json.dumps(data))
+    assert response.status_code == 404
+    userId = 0
+    data["email"] = '101010.com'
+    response = requests.put(BASE_URL + f"user/updateDetails/{userId}", data=json.dumps(data))
+    assert response.status_code == 422
